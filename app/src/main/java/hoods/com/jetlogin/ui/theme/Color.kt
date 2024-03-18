399126a530c46081c5a943e59b9493cb6f8abfc2
maxohm1 <maxohm24@gmail.com>
2024-03-18 14:40:53 +0530

    Login and signup

---
+++ app/src/main/java/hoods/com/jetlogin/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package hoods.com.jetlogin.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFFEFEFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFFE2E2E2)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Pink60 = Color(0xFFF03A53)
+val DarkBlue60 = Color(0xFFB8BBD5)
+val Brown40 = Color(0XFFad8b5c)
